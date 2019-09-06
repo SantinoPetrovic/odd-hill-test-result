1763975158015652a83d33aa6ead1201f51c3336
Santino Petrovic <santinopetrovic@gmail.com>
2019-09-06 16:38:05 +0000

    Added express and running it as an app

---
+++ server.js
@@ -0,0 +1,10 @@
+// Get dependencies
+const express = require('express');
+
+// Define app
+const app = express();
+
+// Run app
+app.listen(3000, function(){
+    console.log("Running on port 3000!");
+});
