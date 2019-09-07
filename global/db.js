40bc685fec7ceb1b010af7e51841b418e88583b4
Santino Petrovic <santinopetrovic@gmail.com>
2019-09-07 08:38:33 +0000

    Path change on database.

--- global/db.js
+++ global/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3');
 
 module.exports = function () { 
-    return new sqlite3.Database("../data/bookshelf.sqlite", function(err){
+    return new sqlite3.Database("./data/bookshelf.sqlite", function(err){
         if (err) {
             return err;
         }
